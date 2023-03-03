2b1703c9f5c8d045d6b679c8bebf7cc7202c1f6c
Andrii-Smolianinov <andr380v@gmail.com>
2023-03-03 23:38:00 +0200

    test project

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 const buttonEl = document.getElementById("button");
 const spanEl = document.getElementById("span");
 
-const onButtonClick = () => {
-  buttonEl.classList.toggle("userCard__button--active");
+returnButton();
+buttonEl.addEventListener("click", onButtonClick);
+const re = /\B(?=(\d{3})+(?!\d))/g;
+
+function onButtonClick() {
   const isActive = buttonEl.classList.contains("userCard__button--active");
 
   if (!isActive) {
-    spanEl.textContent = Number(spanEl.textContent) - 1;
-    buttonEl.innerText = "follow";
-  } else {
-    spanEl.textContent = Number(spanEl.textContent) + 1;
     buttonEl.innerText = "following";
+    localStorage.setItem("Button", "following");
+    buttonEl.classList.remove("userCard__button");
+    buttonEl.classList.add("userCard__button--active");
+    spanEl.textContent = Number(spanEl.textContent) + 1;    
+  } else {
+    buttonEl.innerText = "follow";
+    localStorage.setItem("Button", "follow");
+    buttonEl.classList.remove("userCard__button--active");
+    buttonEl.classList.add("userCard__button");
+    spanEl.textContent = Number(spanEl.textContent) - 1;    
   }
-};
+}
 
-buttonEl.addEventListener("click", onButtonClick);
+function returnButton() {
+  const buttonMessage = localStorage.getItem("Button");
 
+  if (buttonMessage) {
+    buttonEl.innerText = buttonMessage;
+  }
+
+  if (buttonMessage === "follow") {
+    buttonEl.classList.add("userCard__button");
+  } else {
+    buttonEl.classList.add("userCard__button--active");
+    spanEl.textContent = Number(spanEl.textContent) + 1;
+  }
+}
 
 // const re = /\B(?=(\d{3})+(?!\d))/g;
 // const followers = spanEl.textContent;
