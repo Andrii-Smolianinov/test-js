d0759a92eb14364cd979804713b89157333432c6
Andrii-Smolianinov <andr380v@gmail.com>
2023-03-05 13:22:48 +0200

    test js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const buttonEl = document.getElementById("button");
 const spanEl = document.getElementById("span");
 
 returnButton();
+
 buttonEl.addEventListener("click", onButtonClick);
-const re = /\B(?=(\d{3})+(?!\d))/g;
 
 function onButtonClick() {
   const isActive = buttonEl.classList.contains("userCard__button--active");
@@ -13,13 +13,13 @@ function onButtonClick() {
     localStorage.setItem("Button", "following");
     buttonEl.classList.remove("userCard__button");
     buttonEl.classList.add("userCard__button--active");
-    spanEl.textContent = Number(spanEl.textContent) + 1;    
+    spanEl.textContent = Number(spanEl.textContent) + 1;
   } else {
     buttonEl.innerText = "follow";
     localStorage.setItem("Button", "follow");
     buttonEl.classList.remove("userCard__button--active");
     buttonEl.classList.add("userCard__button");
-    spanEl.textContent = Number(spanEl.textContent) - 1;    
+    spanEl.textContent = Number(spanEl.textContent) - 1;
   }
 }
 
@@ -38,6 +38,14 @@ function returnButton() {
   }
 }
 
-// const re = /\B(?=(\d{3})+(?!\d))/g;
-// const followers = spanEl.textContent;
-// const replaceFollowers = followers.replace(re, ",");
+function changeNumberFormat(number) {
+  const numberFormat = new Intl.NumberFormat("en-GB");
+  const formatFollowers = numberFormat.format(number);
+  return formatFollowers;
+}
+
+function reChangeNumberFormat(number) {
+  const numberFormat = new Intl.NumberFormat();
+  const reFormatFollowers = numberFormat.format(number);
+  return reFormatFollowers;
+}
